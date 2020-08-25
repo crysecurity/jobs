d7021f5b05cfae0294cd00676f8d91901cb4bd66
Cry Security <cr4sec@gmail.com>
2020-08-25 18:48:19 +0300

    code style

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -2,8 +2,19 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Currency
+ * @package App\Models
+ * @property-read int $id
+ * @property string $name
+ * @property string $code
+ * @property string $symbol
+ * @property-read Carbon $created_at
+ * @property-read Carbon $updated_at
+ */
 class Currency extends Model
 {
     protected $table = 'currency';
